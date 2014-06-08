6f661708916a3a9044876bcf274a8473e78b4cdc
Gian Carlo Pace <giancarlo.pace@gmail.com>
2014-06-08 12:49:45 +0200

    graphic review

--- app/assets/javascripts/selfies.js.coffee
+++ app/assets/javascripts/selfies.js.coffee
@@ -1,6 +1,6 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-$ () ->
-  wall = new freewall("#selfies");
-  wall.fitWidth();
+# $ ()->
+#   wall = new freewall("#container");
+#   wall.fitWidth();
--- app/models/selfie.rb
+++ app/models/selfie.rb
@@ -1,7 +1,7 @@
 class Selfie < ActiveRecord::Base
   belongs_to :event
   has_attached_file :image,
-    :styles => { :thumb=> "100x100#", :small  => "300x300>", :large => "600x600>"}, :default_url => "/images/:style/missing.png"
+    :styles => { thumb: "100x100#", selfie_format: "210x210#", small: "300x300>", large: "600x600>"}, :default_url => "/images/:style/missing.png"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
